62b3f7b07e4301b40b79b017fae2244ef96d98f2
조우리 <whdnfl0504@naver.com>
2022-07-20 15:25:27 +0900

    220720

---
+++ Assets/1.Script/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float hAxis;
+    float vAxis;
+    [SerializeField] float speed;
+
+    void Start()
+    {
+      
+    }
+
+    void Update()
+    {
+        hAxis=Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+        if (moveVec.magnitude!=0)        
+            moveMoment(moveVec);
+        
+    }
+
+    void moveMoment(Vector3 dir)
+    {
+        Vector3 lookdir=transform.TransformDirection(dir);
+        lookdir.y = 0;
+        transform.Translate(dir * speed * Time.deltaTime);
+        Quaternion q=Quaternion.LookRotation(lookdir,Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q,10*Time.deltaTime);
+
+    }
+}
